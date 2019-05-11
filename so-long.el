2e4905986509dc086e97cfc25cfe1ee8e9382fea
Phil Sainty <psainty@orcon.net.nz>
2019-05-12 00:38:24 +1200

    Nicer `describe-key' output for `so-long-menu-item-replace-action'

--- so-long.el
+++ so-long.el
@@ -849,9 +849,13 @@ If RESET is non-nil, remove any existing values before storing the new ones."
           item
         (define-key-after map (vector key)
           `(menu-item
-            ,label (lambda ()
-                     (interactive)
-                     (so-long-menu-item-replace-action ',item))
+            ,label
+            ,(let ((sym (make-symbol "so-long-menu-item-replace-action")))
+               (defalias sym
+                 (apply-partially #'so-long-menu-item-replace-action item)
+                 (documentation #'so-long-menu-item-replace-action))
+               (put sym 'interactive-form '(interactive))
+               sym)
             :enable (not (and so-long--active
                               (eq ',actionfunc so-long-function)
                               (eq ',revertfunc so-long-revert-function)))))))
